7726156b259fff414c196e603bce91ceeacc4333
jules <you@example.comjulesbanning@gmail.com>
2015-11-28 16:58:49 +0100

    Added views to create/edit blog post inside the site.

--- mysite/urls.py
+++ mysite/urls.py
@@ -3,5 +3,6 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^logout/$', 'django.contrib.auth.views.logout', {'next_page': '/'},name="logout"),
     url(r'', include('blog.urls')),
 ]
